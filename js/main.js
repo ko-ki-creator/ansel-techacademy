7450995af5e545991e577f3b39b144077c7f087a
koki sugano <c9xj.c9ajmf2@gmail.com>
2025-05-23 13:19:47 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -16,20 +16,3 @@ btn.addEventListener("click", () => {
         behavior: "smooth",
     });
 });
-
-
-// フェードイン
-//scroll_effect
-$(window).scroll(function () {
-    var scrollAnimationElm = document.querySelectorAll('faid_in');
-    var scrollAnimationFunc = function () {
-        for (var i = 0; i < scrollAnimationElm.length; i++) {
-            var triggerMargin = 100;
-            if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
-                scrollAnimationElm[i].classList.add('on');
-            }
-        }
-    }
-    window.addEventListener('load', scrollAnimationFunc);
-    window.addEventListener('scroll', scrollAnimationFunc);
-});
