974ea2dd58565f6ecd6a5c74896507147b1ee80a
koki sugano <c9xj.c9ajmf2@gmail.com>
2025-10-13 18:31:01 +0900

    animation_add

--- js/main.js
+++ js/main.js
@@ -16,3 +16,28 @@ btn.addEventListener("click", () => {
         behavior: "smooth",
     });
 });
+
+
+// トリガー取得
+const scrollTrigger = document.querySelectorAll('.js-scroll-trigger');
+
+// aimation呼び出し
+if (scrollTrigger.length) {
+    scrollAnimation(scrollTrigger);
+}
+
+// aimation関数
+function scrollAnimation(trigger) {
+    window.addEventListener('scroll', function () {
+        for (var i = 0; i < trigger.length; i++) {
+            let position = trigger[i].getBoundingClientRect().top,
+                scroll = window.pageYOffset || document.documentElement.scrollTop,
+                offset = position + scroll,
+                windowHeight = window.innerHeight;
+
+            if (scroll > offset - windowHeight + 200) {
+                trigger[i].classList.add('is-active');
+            }
+        }
+    });
+}
